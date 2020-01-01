b58c7e3daf0e21f4b5c2caeee5600b3a674ae069
Terry Laptop <tellaway@sky.com>
2020-01-01 20:00:49 +0000

    Start

---
+++ MMM-MyWeatherData.js
@@ -0,0 +1,86 @@
+'use strict';
+
+Module.register("MMM-MyWeatherData", {
+
+  result: {},
+  defaults: {
+    fiat: 'usd',
+    showBefore: null,
+    exchange: 'bitstamp',
+    updateInterval: 60000,
+
+    // Used to work out url and symbols
+    fiatTable: {
+      usd: {
+        symbol: '$',
+        exchangeCode: 'btcusd'
+      },
+      eur: {
+        symbol: '€',
+        exchangeCode: 'btceur'
+      }
+    }
+  },
+
+  getStyles: function() {
+    return ["MMM-MyWeatherData.css"];
+  },
+
+  start: function() {
+    this.getTickers();
+    this.scheduleUpdate();
+  },
+
+  getDom: function() {
+    var wrapper = document.createElement("ticker");
+    wrapper.className = 'medium bright';
+    wrapper.className = 'ticker';
+
+    var data = this.result;
+    var symbolElement =  document.createElement("span");
+    var exchange = this.config.exchange;
+    var fiat = this.config.fiat;
+    var fiatSymbol = this.config.fiatTable[fiat].symbol;
+    var lastPrice = data.last;
+    if (this.config.showBefore == null) {
+      var showBefore = this.config.exchange;
+    } else {
+      var showBefore = this.config.showBefore
+    }
+    if (lastPrice) {
+      symbolElement.innerHTML = showBefore + ' ' + fiatSymbol;
+      wrapper.appendChild(symbolElement);
+      var priceElement = document.createElement("span");
+      priceElement.innerHTML = lastPrice;
+      wrapper.appendChild(priceElement);
+    }
+    return wrapper;
+  },
+
+  scheduleUpdate: function(delay) {
+    var nextLoad = this.config.updateInterval;
+    if (typeof delay !== "undefined" && delay >= 0) {
+      nextLoad = delay;
+    }
+
+    var self = this;
+    setInterval(function() {
+      self.getTickers();
+    }, nextLoad);
+  },
+
+  getTickers: function () {
+    var fiat = this.config.fiat;
+    var url = 'https://192.168.0.40/0';
+    this.sendSocketNotification('GET_TICKERS', url);
+  },
+
+  socketNotificationReceived: function(notification, payload) {
+    if (notification === "TICKERS_RESULT") {
+      var self = this;
+      this.result = payload;
+      this.updateDom(self.config.fadeSpeed);
+    }
+  },
+
+});
